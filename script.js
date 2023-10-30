ebfe50307baffc1284147cf09a33d6ec9acfd8c8
Arkinaz <gd7nttqqx@code.edu.az>
2023-10-30 19:52:31 +0400

    aaa

---
+++ script.js
@@ -0,0 +1,49 @@
+let firstName = "Arkinaz";
+let lastName = "Mammadova";
+let fullName = lastName + " " + firstName;
+
+console.log(fullName);
+
+let country = "Azerbaidjan";
+let age = "18";
+let job = "student";
+
+console.log("I'm from " + country + ". My age is " + age + " and im a " + job);
+
+let s = 16; //number
+console.log(typeof s);
+let color = "brown"; //string
+console.log(typeof color);
+let float = "12.5"; //number
+console.log(typeof float);
+let like = true; //boolean
+console.log(typeof like);
+
+let x = {
+  firstName: "nick",
+  color: "red",
+}; //object
+console.log(typeof x);
+
+let lenght = 8600;
+let speed = 120;
+let time = (lenght / speed) * 60;
+
+console.log("The car will arrive in china in " + time + ".");
+
+let yearFirst = 18;
+let yearSecond = 18 + 1;
+
+console.log("I will be aither " + yearFirst + " or " + yearSecond + ".");
+
+
+let leman = 80;
+let qenire = 90;
+let nergiz = 100;
+let amongus = 120;
+let ortalama = (leman+qenire+nergiz+amongus)/4;
+
+console.log("Ortalama: " + ortalama);
+
+
+let a = leman 
