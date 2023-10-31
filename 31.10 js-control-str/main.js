516f7a0091de3b101571d6b4957173720d406a8c
Arkinaz <gd7nttqqx@code.edu.az>
2023-10-31 19:57:05 +0400

    done

---
+++ 31.10 js-control-str/main.js
@@ -0,0 +1,89 @@
+// TASK 1 !
+
+// let weather=70;
+
+// if (weather > 22) {
+//   console.log(`Hava cok sicak`);
+// } else if (weather > 15) {
+//   console.log(`Usumuyormusun?
+// `);
+// } else { console.log(`Donuyorum`);
+// }
+
+// TASK 2
+
+// let letter = "b";
+
+// if (
+//   letter === "a" ||
+//   letter === "e" ||
+//   letter === "i" ||
+//   letter === "u" ||
+//   letter === "o"
+// ) {
+//   console.log(`Bu saitdir`);
+// }
+//  else {
+//   console.log(`Bu samiitdir`);
+// }
+
+// TASK 3
+
+// let a = 25;
+// let b = 52;
+// let c = 88;
+
+// if(a>b && a>c){console.log(`${a} boyuk ededddir`);}
+// else if(b>a && b>c){console.log(`${b} boyuk ededddir`);}
+// else{console.log(`${c} boyuk ededddir`);}
+
+// TASK 4
+
+// let exam = 100;
+
+// if(exam > 90){console.log(`Exam grade: A`);}
+// else if(exam > 80){console.log(`Exam grade: B`);}
+// else if(exam > 70){console.log(`Exam grade: C`);}
+// else if(exam > 60){console.log(`Exam grade: D`);}
+// else{console.log(`Exam grade: F`);}
+
+// TASK 5
+
+// let userName = "Fidan";
+
+// if (userName.length <= 5) {
+//   console.log(`Salam ${userName}, qisa adiniz vaz.`);
+// } else if (userName.length <= 10) {
+//   console.log(`Salam ${userName}, orta uzunluqlu adiniz vaz.`);
+// } else {
+//   console.log(`Salam ${userName}, adiniz cox uzundur.`);
+// }
+
+// TASK 6
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// if (a + b > b + c && a + b > a + c) {
+//  console.log( `big sum result ${a} and ${b}.`);;
+// } else if (b + c > a + c && b + c > a + b) {
+//   console.log(`big sum result ${c} and ${b}.`);;
+// } else {
+//   console.log(`big sum result ${c} and ${a}.`);;
+// }
+
+// TASK 7
+let a = 4;
+let b = 5;
+let c = 6;
+
+if (c>=a && c>=b ) {
+    if(b>a){console.log(`big number ${c*100+ b*10 +a} little number ${a*100+b*10+c}`);}
+    else{console.log(`big number ${c*100+ a*10 +b} little number ${b*100+a*10+c}`);}
+}
+else if(a>b && a>c){
+    
+}
+
+
