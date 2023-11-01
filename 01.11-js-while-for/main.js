c9d980ff379164f388337b527c120cf9dddb04b6
Arkinaz <gd7nttqqx@code.edu.az>
2023-11-01 19:22:22 +0400

    done

---
+++ 01.11-js-while-for/main.js
@@ -0,0 +1,68 @@
+// 1
+// for (let i=1; i < 100; i++) {
+//   if (i % 2 == 0) console.log(i);
+// }
+
+// 2
+
+// let str = `arkinaz mammadova`;
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+
+// 3
+// let i=0;
+// for(let i=10;i<100; i++){
+//     if(i%10===7)
+//     console.log(i);
+// }
+
+// 4
+
+// let i=0;
+
+// for(i=10;i<100;i++){
+//     if(i%11===0)console.log(i);
+// }
+
+
+// 5
+// let i=9;
+
+
+// for(let a=0;a<=i;a++){
+//     if(i%a===0)console.log(a);
+// }
+
+// 6
+
+// let i=3;
+// let multi =1;
+// for(let a=1;a<=i;a++){
+//     multi=multi*a
+//     console.log(multi);
+// }
+
+
+// 7
+
+
+// let sum=0;
+// for(let i=0;i<10;i++){
+//     if(i%2==0)
+//     sum=sum+i
+//     console.log(sum);
+// }
+
+// 8
+
+// let sum =0;
+// for(let i=10;i<30;i++){
+//     if(i%3===0)
+//     sum=sum+i
+    
+// }
+// console.log(sum);
+
