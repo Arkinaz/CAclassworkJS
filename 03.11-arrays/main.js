edab45d9e9a470f755b4fc3c6aaa9e86c9df3a35
Arkinaz <gd7nttqqx@code.edu.az>
2023-11-03 20:05:37 +0400

    done

---
+++ 03.11-arrays/main.js
@@ -0,0 +1,72 @@
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+
+// let max = numbers[0];
+// let min = numbers[0];
+
+// for (i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+//   else if (numbers[i] < min) {
+//     min = numbers[i];
+//   }
+// }
+
+// console.log("En boyuk eded : ", max);
+// console.log("En kicik eded : ", min);
+
+//---------
+
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+
+// let sum = 0;
+
+// for (i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(sum);
+
+//---------
+
+// let numbers = [12, -45, 6, -78, 34, -99, 23];
+// let pos = [];
+// let sum = 0;
+
+// for (i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 0) {
+//     sum += numbers[i];
+//     pos.push(numbers[i]);
+//   }
+// }
+// console.log(pos);
+// console.log(sum);
+
+
+//----------
+
+// let ogArray =[1,2,3,4];
+
+// let repeatArray=[];
+
+// let n =3;
+
+// for(let i =0; i<ogArray.length;i++){
+//     for(let j=0;j<n;j++){
+//        repeatArray.push(ogArray[i])
+//     }
+// }
+
+// console.log(repeatArray);
+
+
+//----------
+
+let numbers = [12, 45, 6, 78, 34, 99, 23];
+
+let tek;
+let cut;
+
+for(let i =0; i<numbers.length;i++){
+
+    
+}
