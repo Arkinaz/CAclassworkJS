4e4bdfdaff68266e72b738462390e36878d1418e
Arkinaz <gd7nttqqx@code.edu.az>
2023-11-15 19:49:44 +0400

    done

---
+++ 15-object methods/main.js
@@ -0,0 +1,91 @@
+let meOmg = {
+  nameOfMe: `Amongus`,
+  surNameOfMe: `Potion`,
+  age: `73474878427848734`,
+  skills: [`turning into imposter`, `having knife with me 7/24`, `amogus`],
+  other: `sus`,
+};
+
+// console.log(Object.keys(meOmg));
+//////
+// console.log(Object.values(meOmg));
+//////
+
+// let arr = Object.entries(meOmg)
+
+// console.log(arr.map((item)=>`${item[0]}: ${item[1]}`));;
+
+// console.log(arr);
+
+////
+
+// delete meOmg.age;
+
+// console.log(meOmg);
+
+//////
+
+// let arr = Object.values(meOmg);
+
+// let max = arr[0];
+
+// let result = arr.forEach((item) => {item.length > max.length && max === item})
+
+// console.log(max);
+
+/////////
+
+// var library = [
+//   {
+//     title: "The Road Ahead",
+//     author: "Bill Gates",
+//     readingStatus: true,
+//   },
+//   {
+//     title: "Walter Isaacson",
+//     author: "Steve Jobs",
+//     readingStatus: true,
+//   },
+//   {
+//     title: "Suzanne Collins",
+//     author: "Mockingjay: The Final Book",
+//     readingStatus: false,
+//   },
+// ];
+
+//   "library" adlı array yaradın. İçərisində title, author və readingStatus adlı key-ləri olan objectlər yaradın.
+// Əgər readingStatus true olarsa console-a "Already read 'title' by author' Əgər false
+// olarsa "You still need to read 'title' by author' yazsın.
+
+
+// library.forEach((item) => {
+//   if ((item.readingStatus === true)) {
+//     console.log(`Already read ${item.title} by ${item.author}.`);;
+//   } else {
+//     console.log(`You still need to read ${item.title} by ${item.author}`);;
+//   }
+// });
+
+////////
+
+let employee = {
+    name: 'aaa',
+    jobTitle: 'bbb',
+    salary:123 ,
+    promote: function(p1, p2) {
+        
+    }
+  };
+
+// "employee" adlı object yaradın. Object-in name, jobTitle, salary və promote adında key-ləri var.
+// promote bir anonim function-dır, 2 parametr qəbul edir. jobTitle-in value-ı ilk dəfə "Software Developer" salary isə 75000.
+// Ən sonda employee.promote-a bir yeni vəzifə və yeni maaş göndərin.
+
+// employee.promote('Senior Software Engineer', 90000);
+
+// Nəticə: Promotion: Alice is now Senior Software Engineer with a salary of $90000
+
+
+
+
+
