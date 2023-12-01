89136af6ef6d78bc254ab708179e625e38fae761
Arkinaz <gd7nttqqx@code.edu.az>
2023-12-01 18:13:13 +0400

    done

---
+++ Notes thing but dif/main.js
@@ -0,0 +1,12 @@
+// .css({
+//   backgroundColor: "cyan",
+//   border: "1px solid blue",
+//   borderRadius: "5px",
+//   width: "fit-content",
+//   padding: "5px",
+// });
+
+let arr =  [];
+
+
+
